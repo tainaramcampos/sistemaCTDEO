68f93c013a7df5ad81b16cbe10a16ef4eca96d89
tainaramcampos <tainaramcampos@gmail.com>
2018-09-11 20:20:43 -0300

    correção nas labels da view de turma

---
+++ WebApplicationCTDEO/Migrations/201809092244230_modificacao_de_nivel1.cs
@@ -0,0 +1,16 @@
+namespace WebApplicationCTDEO.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class modificacao_de_nivel1 : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
--- WebApplicationCTDEO/Models/Modalidade.cs
+++ WebApplicationCTDEO/Models/Modalidade.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Web;
 
@@ -8,19 +9,38 @@ namespace WebApplicationCTDEO.Models
     public class Modalidade
     {
         public int ModalidadeId { get; set; }
+
+        [Display(Name = "Modalidade")]
         public string Nome { get; set; }
+
+        [Display(Name = "Nível")]
         public NivelModalidade? Nivel { get; set; }
+
+        [Display(Name = "Grupo")]
         public GruposAtletismo? GrupoAtl { get; set; }
     }
     
     
     public enum NivelModalidade
     {
-        T1Adaptacao, T1Adequacao, T2Desenvolvimento, T3Aperfeicoamento
+        [Display(Name = "T1 - Adaptação")] T1Adaptacao,
+        [Display(Name = "T1 - Adequação")]
+        T1Adequacao,
+        [Display(Name = "T2 - Adequação")]
+        T2Adequacao,
+        [Display(Name = "T2 - Desenvolvimento")]
+        T2Desenvolvimento,
+        [Display(Name = "T3 - Aperfeiçoamento")]
+        T3Aperfeicoamento
     }
 
     public enum GruposAtletismo
     {
-        VelocidadeeSaltos, ArremessoeLançamento, FundoMeiofundo 
+        [Display(Name = "Velocidade e Saltos")]
+        VelocidadeeSaltos,
+        [Display(Name = "Arremesso e Lançamento")]
+        ArremessoeLançamento,
+        [Display(Name = "Meio-Fundo e Fundo")]
+        FundoMeiofundo 
     }
 }
