bcc936816e8006c52d20ba31de414d47abebdf34
tainaramcampos <tainaramcampos@gmail.com>
2018-09-15 07:56:55 -0300

    Edição de aluno

--- WebApplicationCTDEO/Controllers/AlunosController.cs
+++ WebApplicationCTDEO/Controllers/AlunosController.cs
@@ -50,6 +50,8 @@ namespace WebApplicationCTDEO.Controllers
            /* [Bind(Include = "CPF,Nome,Profissao,GraudeParentesco, IsResponsavel")] Familiar familiar*/)
 
         {
+            //reenviar dados caso a página tenha que ser recarregada
+            ViewData["Modalidades"] = db.Modalidade.ToList();
             ViewData["Turmas"] = db.Turmas.ToList();
             Turma turmaModel = new Turma();
 
@@ -120,6 +122,8 @@ namespace WebApplicationCTDEO.Controllers
         // GET: Alunos/Edit/5
         public ActionResult Edit(int? id)
         {
+            ViewData["Turmas"] = db.Turmas.ToList();
+            ViewData["Modalidades"] = db.Modalidade.ToList();
             if (id == null)
             {
                 return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
@@ -137,14 +141,49 @@ namespace WebApplicationCTDEO.Controllers
         // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit([Bind(Include = "AlunoId,Nome,Genero,DatadeNascimento,Procedencia,CPF,RG,OrgaoExp,DatadeExp,Endereco,Numero,Complemento,Bairro,Municipio,Comunidade,CEP,TelefoneResidencial,Celular,InstituicaodeEnsino,RededeEnsino,CRE,BolsadeEstudos,TipodeInstituicao,Serie,Turno,Transporte,RegistroFed,TipodeBolsaAtleta")] Aluno aluno)
+        public ActionResult Edit([Bind(Include = "AlunoId,Nome,Genero,DatadeNascimento,Procedencia,CPF,RG,OrgaoExp,DatadeExp,Endereco,Numero,Complemento,Bairro,Municipio,Comunidade,CEP,TelefoneResidencial,Celular,InstituicaodeEnsino,RededeEnsino,CRE,BolsadeEstudos,TipodeInstituicao,Serie,Turno,Transporte,RegistroFed,TipodeBolsaAtleta,IdsdeTurmas")] Aluno aluno)
         {
+            ViewData["Turmas"] = db.Turmas.ToList();
+            ViewData["Modalidades"] = db.Modalidade.ToList();
+            
             if (ModelState.IsValid)
             {
-                db.Entry(aluno).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
+
+                using (var context = new DatabaseContext())
+                {
+                    if (aluno.IdsdeTurmas != null) //se tiver alguma turma na lista, salvar na tabela
+                    {
+                            //lista para receber os códigos de turmas selecionadas
+                            List<string> stringTurmas = new List<string>();
+                            stringTurmas = aluno.IdsdeTurmas.Split(',').ToList(); //adiciona a string de ids de turmas numa lista
+                            stringTurmas.Remove(""); //remove os itens em branco
+
+
+                            foreach (var item in stringTurmas)
+                            {
+                                int id = Int32.Parse(item); //convertendo os ids de string pra int
+
+                                //resagatando as turmas existentes
+                                var turma = context.Turmas.Include("Alunos")
+                                .Where(s => s.TurmaId == id).FirstOrDefault<Turma>();
+
+                                //attach
+                                if (context.Entry(turma).State == EntityState.Detached)
+                                    context.Turmas.Attach(turma);
+
+                                //adicionando na lista de alunos
+                                aluno.Turmas.Add(turma);
+                            }
+                    }
+                    context.Alunos.Add(aluno);
+                    //context.Alunos.Attach(aluno);
+                    context.Entry(aluno).State = EntityState.Modified;
+                    context.SaveChanges();
+                }
+
+            return RedirectToAction("IndexAlunos");
+        }
+            
             return View(aluno);
         }
 
